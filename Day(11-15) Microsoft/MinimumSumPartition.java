50372a51ed8276c790d5fa10b068a14f78c92c5b
Ashwany Kumar Verma <33549837+ashwin2567@users.noreply.github.com>
2022-01-12 12:21:40 +0530

    Create MinimumSumPartition.java

---
+++ Day(11-15) Microsoft/MinimumSumPartition.java
@@ -0,0 +1,22 @@
+class Solution
+{
+   static int min = Integer.MAX_VALUE;
+	public int minDifference(int arr[], int n) 
+	{ 
+	   func(arr, 0, 0, 0);
+	   return min;
+	} 
+	
+	static void func(int arr[], int index, int sum1, int sum2)
+	{
+	    if(index == arr.length)
+	    {
+	        int diff = Math.abs(sum1 - sum2);
+	        min = Math.min(min, diff);
+	        return;
+	    }
+	    
+	    func(arr, index + 1, sum1 + arr[index], sum2);
+	    func(arr, index + 1, sum1, sum2 + arr[index]);
+	}
+}
