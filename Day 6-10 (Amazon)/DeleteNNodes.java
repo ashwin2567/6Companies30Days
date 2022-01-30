d475c54eb489b4da6da101afec582937425a5099
Ashwany Kumar Verma <33549837+ashwin2567@users.noreply.github.com>
2022-01-30 14:58:42 +0530

    Create DeleteNNodes.java

---
+++ Day 6-10 (Amazon)/DeleteNNodes.java
@@ -0,0 +1,24 @@
+class Solution
+{
+   static void linkdelete(Node head, int M, int N)
+   {
+       Node curr = head;
+       int count =0;
+       while(curr!=null && curr.next!=null ){
+           count++;
+           int x = N;
+           if(count==M){
+               while(x>0){
+               curr.next = curr.next.next;
+               if(curr.next==null){
+                   return;
+               }
+               count=0;
+               x--;
+           }
+           x=N;
+           }
+           curr = curr.next;
+       }
+   }
+}
