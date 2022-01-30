b34fbdca7d7dcd9b587aff5244743e012f3bbe52
Ashwany Kumar Verma <33549837+ashwin2567@users.noreply.github.com>
2022-01-30 14:59:14 +0530

    Update DeleteNNodes.java

--- Day 6-10 (Amazon)/DeleteNNodes.java
+++ Day 6-10 (Amazon)/DeleteNNodes.java
@@ -4,21 +4,25 @@ class Solution
    {
        Node curr = head;
        int count =0;
-       while(curr!=null && curr.next!=null ){
+       while(curr!=null && curr.next!=null )
+       {
            count++;
            int x = N;
-           if(count==M){
-               while(x>0){
-               curr.next = curr.next.next;
-               if(curr.next==null){
+           if(count==M)
+           {
+               while(x>0)
+               {
+                   curr.next = curr.next.next;
+                   if(curr.next==null)
+                   {
                    return;
-               }
-               count=0;
-               x--;
-           }
-           x=N;
-           }
-           curr = curr.next;
-       }
-   }
+                   }
+                   count=0;
+                   x--;
+                }
+                x=N;
+            }
+            curr = curr.next;
+        }
+    }
 }
